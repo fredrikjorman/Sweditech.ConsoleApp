ccf9778056e4763967030ffedafd1e85e794c827
ali <ali.bahr2010@icloud.com>
2026-09-04 16:00:51 +0200

    Add dependency injection for supplier

--- Sweditech.ConsoleApp/Program.cs
+++ Sweditech.ConsoleApp/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sweditech.ConsoleApp.Suppliers;
+
+var services = new ServiceCollection();
+
+services.AddSingleton<ISupplierService, SupplierService>();
+services.AddTransient<ISupplierDialog, SupplierDialogService>();
+
+var serviceProvider = services.BuildServiceProvider();
+var supplierDialog = serviceProvider.GetRequiredService<ISupplierDialog>();
+
+supplierDialog.AddSupplier();
