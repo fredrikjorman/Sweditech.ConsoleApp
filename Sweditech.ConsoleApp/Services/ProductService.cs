9cdb8852cd4f6dae2d3507960244adb753938f07
valerio0307 <valer@LAPTOP-89L3H3FH>
2026-09-04 14:54:46 +0200

    Create product and price list

--- Sweditech.ConsoleApp/Services/ProductService.cs
+++ Sweditech.ConsoleApp/Services/ProductService.cs
@@ -1,7 +1,16 @@
 ﻿
+using Sweditech.ConsoleApp.Models;
 namespace Sweditech.ConsoleApp.Services;
 
-    internal class ProductService
+internal class ProductService : IProductService
+
+{
+
+    private readonly List<Product> _products = [];
+    public void AddProduct(Product product)
     {
+
+        _products.Add(product);
     }
+}
 
