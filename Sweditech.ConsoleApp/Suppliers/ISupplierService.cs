4be261f3db393586fc381496d5edf62417e2869f
ali <ali.bahr2010@icloud.com>
2026-09-04 15:13:51 +0200

    Add supplier service

---
+++ Sweditech.ConsoleApp/Suppliers/ISupplierService.cs
@@ -0,0 +1,7 @@
+﻿namespace Sweditech.ConsoleApp.Suppliers;
+
+public interface ISupplierService
+{
+    void AddSupplier(Supplier supplier);
+    List<Supplier> GetSuppliers();
+}
---
+++ Sweditech.ConsoleApp/Suppliers/SupplierService.cs
@@ -0,0 +1,16 @@
+﻿namespace Sweditech.ConsoleApp.Suppliers;
+
+public class SupplierService : ISupplierService
+{
+    private List<Supplier> _suppliers = new List<Supplier>();
+
+    public void AddSupplier(Supplier supplier)
+    {
+        _suppliers.Add(supplier);
+    }
+
+    public List<Supplier> GetSuppliers()
+    {
+        return _suppliers;
+    }
+}
